e76e717702bb84763546ab846c230f4ec112d8d8
Guest User <Guest@Epicodus-10E.local>
2017-09-15 09:56:12 -0700

    Add javascript to buttons and submit form. if statement breaks button, debugging in process.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
 $(document).ready(function(){
-  
+  $("button#bName").click(function(){
+    $("#qOne").slideToggle();
+    $("#qName").slideToggle();
+  });
+  $("button#bOne").click(function(){
+    $("#qOne").slideToggle();
+    $("#qTwo").slideToggle();
+  });
+  $("button#bTwo").click(function(){
+    $("#qTwo").slideToggle();
+    $("#qThree").slideToggle();
+  });
+  $("button#bThree").click(function(){
+    $("#qThree").slideToggle();
+    $("#qFour").slideToggle();
+  });
+  $("button#bFour").click(function(){
+    $("#qFour").slideToggle();
+    $("#qFive").slideToggle();
+  });
+  $("form#track").submit(function(event){
+    $("#qFive").slideToggle();
+    $("#title").slideToggle();
+    $("#final").slideToggle();
+    event.preventDefault();
+    var work = parseInt($("input:radio[name=work]:checked").val());
+    var think = parseInt($("input:radio[name=think]:checked").val());
+    var place = parseInt($("input:radio[name=place]:checked").val());
+    var reason = parseInt($("input:radio[name=reason]:checked").val());
+    var exp = parseInt($("input:radio[name=exp]:checked").val());
+    var result = work + think + place + reason + exp;
+
+    if (result >= 5 && result <== 11) {
+      //$("#cSharp").show();
+    }
+  });
 });
