085520e7e6c7895eccf28f41e17e5a782ca25652
Guest User <Guest@Epicodus-10E.local>
2017-09-15 12:49:32 -0700

    Make enter button press each button. Still submits early for every press of enter.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-  /*This stops people from pressing enter and getting results before they've answered the questions. They have to click the buttons. I don't know how it works, but it does... so I'm just not going to touch it. I was trying to get enter to work for one button at a time but this works too?*/
-
   //keycode 13 = enter
   $("#name").keyup(function(event){
     if(event.keyCode === 13){
@@ -55,40 +53,49 @@ $(document).ready(function(){
   });
   //Submits all values and toggles to results.
   $("form#track").submit(function(event){
-    $("#qFive").slideToggle();
-    $("#title").slideToggle();
-    $("#final").slideToggle();
-    event.preventDefault();
-    var work = parseInt($("input:radio[name=work]:checked").val());
-    var think = parseInt($("input:radio[name=think]:checked").val());
-    var place = parseInt($("input:radio[name=place]:checked").val());
-    var reason = parseInt($("input:radio[name=reason]:checked").val());
-    var exp = parseInt($("input:radio[name=exp]:checked").val());
-    var name = $("input#name").val();
-//Check if they selected any same value 3 times or more.
-    var checkOne = work + think + place;
-    var checkTwo = work + think + reason;
-    var checkThree = work + think + exp;
-    var checkFour = think + place + reason;
-    var checkFive = think + place + exp;
-    var checkSix = place + reason + exp;
+    //Stops enter key from submitting, but also refreshes the entire form...
+    //$("form#track").keyup(function(event){
+      //if(event.keyCode === 13){
+        //event.preventDefault();
+        //return false;
+      //} else {
+        $("#qFive").slideToggle();
+        $("#title").slideToggle();
+        $("#final").slideToggle();
+        event.preventDefault();
+        var work = parseInt($("input:radio[name=work]:checked").val());
+        var think = parseInt($("input:radio[name=think]:checked").val());
+        var place = parseInt($("input:radio[name=place]:checked").val());
+        var reason = parseInt($("input:radio[name=reason]:checked").val());
+        var exp = parseInt($("input:radio[name=exp]:checked").val());
+        var name = $("input#name").val();
+    //Check if they selected any same value 3 times or more.
+        var checkOne = work + think + place;
+        var checkTwo = work + think + reason;
+        var checkThree = work + think + exp;
+        var checkFour = think + place + reason;
+        var checkFive = think + place + exp;
+        var checkSix = place + reason + exp;
 
-    var result = work + think + place + reason + exp;
-//Check if they chose value 1 or 4, 3 times.
-    if (checkOne === 3 || checkTwo === 3 || checkThree === 3 || checkFour === 3 || checkFive === 3 || checkSix === 3) {
-      $(".cSharp").show();
-      $(".name").text(name)
-    } else if (checkOne === 12 || checkTwo === 12 || checkThree === 12 || checkFour === 12 || checkFive === 12 || checkSix === 12) {
-      $(".rwby").show();
-      $(".name").text(name)
-//Check all options where they could select value 2 or 3, 3 or more times.
-//Logic not sound, they overlap.
-    } else if (result >= 8 && result <=14) {
-      $(".java").show();
-      $(".name").text(name)
-    } else {
-      $(".ceeSS").show();
-      $(".name").text(name)
-    }
+        var result = work + think + place + reason + exp;
+    //Check if they chose value 1 or 4, 3 times.
+        if (checkOne === 3 || checkTwo === 3 || checkThree === 3 || checkFour === 3 || checkFive === 3 || checkSix === 3) {
+          $(".cSharp").show();
+          $(".name").text(name)
+        } else if (checkOne === 12 || checkTwo === 12 || checkThree === 12 || checkFour === 12 || checkFive === 12 || checkSix === 12) {
+          $(".rwby").show();
+          $(".name").text(name)
+    //Check all options where they could select value 2 or 3, 3 or more times.
+    //Logic not sound, they overlap.
+        } else if (result >= 8 && result <=14) {
+          $(".java").show();
+          $(".name").text(name)
+        } else {
+          $(".ceeSS").show();
+          $(".name").text(name)
+        }
+      //}
+    //});
+
   });
 });
